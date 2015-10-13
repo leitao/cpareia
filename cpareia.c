42b1028466c329bb6341d11faac7bd1cd46669ce
Michel Boaventura <michel.boaventura@gmail.com>
2015-10-13 13:54:52 -0300

    Melhora reporte de errors

--- cpareia.c
+++ cpareia.c
@@ -5,12 +5,12 @@ main(int argc,  char *argv[]) {
   project *my_proj;
 
   if(argc != 2)
-    handle_error("argc");
+    handle_error("Usage: cpareia XML");
 
   my_proj = project_new();
 
   project_fill(my_proj, argv[1]);
-  project_print(my_proj);
+  /*project_print(my_proj);*/
 
   project_free(my_proj);
 
