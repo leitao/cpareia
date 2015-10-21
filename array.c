1d9ab1d75b2148c50464a7511674d0e3320e9411
Michel Boaventura <michel.boaventura@gmail.com>
2015-10-21 14:21:59 -0200

    Implementa array_get()
    Com esta função o acesso ao array fica abstraído, facilitando futuras
    mudanças.

--- array.c
+++ array.c
@@ -13,20 +13,20 @@ array_new_prealloc(size_t size) {
 
   ary->size = 0;
   ary->_total_size = size;
-  ary->data = (void **) malloc(sizeof(void *) * size);
+  ary->_data = (void **) malloc(sizeof(void *) * size);
 
   return ary;
 }
 
 void
 array_fini(array *ary) {
-  ary->data = (void **) realloc(ary->data, sizeof(void *) * ary->size);
+  ary->_data = (void **) realloc(ary->_data, sizeof(void *) * ary->size);
   ary->_total_size = ary->size;
 }
 
 void
 array_free(array *ary) {
-  free(ary->data);
+  free(ary->_data);
   free(ary);
 }
 
@@ -34,16 +34,21 @@ void *
 array_remove_last(array *ary) {
   ary->size--;
 
-  return ary->data[ary->size];
+  return ary->_data[ary->size];
 }
 
 void
 array_add(array *ary, void *el) {
   if(ary->size == ary->_total_size) {
     ary->_total_size *= 2;
-    ary->data = (void **)
-      realloc(ary->data, sizeof(void *) * ary->_total_size);
+    ary->_data = (void **)
+      realloc(ary->_data, sizeof(void *) * ary->_total_size);
   }
 
-  ary->data[ary->size++] = el;
+  ary->_data[ary->size++] = el;
+}
+
+inline void *
+array_get(array *ary, size_t i) {
+  return i < ary->size ? ary->_data[i] : NULL;
 }
