ad4a16408c1a0f726dec99abdc30e907761ae955
Michel Boaventura <michel.boaventura@gmail.com>
2015-11-23 15:03:14 -0200

    Implementação de blocagem paralela

--- blocking.c
+++ blocking.c
@@ -30,6 +30,51 @@ blocking_generate_keys(project_t *project, record_t *record) {
   }
 }
 
+void *
+blocking_generate_all_keys(void *data) {
+  size_t i;
+  int rank, total_ranks;
+  project_t *project;
+  blocking_thread_params *param;
+  record_t *record;
+
+  param = data;
+
+  rank = param->rank;
+  project = param->project;
+  total_ranks = param->total_ranks;
+
+  for(i = rank; i < array_total_size(project->d0->records); i += total_ranks) {
+    while(!(record = array_get(project->d0->records, i))) {
+      sleep(0.2);
+    }
+    blocking_generate_keys(project, record);
+  }
+
+  return data;
+}
+
+pthread_t **
+blocking_generate_keys_async(project_t *project, int num_threads) {
+  pthread_t **threads;
+  blocking_thread_params *param;
+  int i;
+
+  threads = malloc(sizeof(pthread_t *) * num_threads);
+
+  for(i = 0; i < num_threads; i++) {
+    threads[i] = malloc(sizeof(pthread_t));
+    param = malloc(sizeof(blocking_thread_params));
+    param->project = project;
+    param->rank = i;
+    param->total_ranks = num_threads;
+
+    pthread_create(threads[i], NULL, blocking_generate_all_keys, param);
+  }
+
+  return threads;
+}
+
 void
 blocking_print(project_t *project) {
   hash_print(project->blocks);
--- blocking.h
+++ blocking.h
@@ -1,13 +1,21 @@
 #ifndef _BLOCKING_H_
 #define _BLOCKING_H_
 
+#include <pthread.h>
+
 #include "array.h"
 #include "record.h"
 #include "project.h"
 #include "field_comparator.h"
 #include "hash.h"
 
+typedef struct blocking_thread_params {
+  project_t *project;
+  int rank, total_ranks;
+} blocking_thread_params;
+
 void blocking_generate_keys(project_t *, record_t *);
+pthread_t **blocking_generate_keys_async(project_t *, int);
 void blocking_print(project_t *);
 
 #endif
