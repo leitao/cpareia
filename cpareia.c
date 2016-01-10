6acf1737bc1c8cb4f009d41f5d95a7b6f94db703
Michel Boaventura <michel.boaventura@gmail.com>
2016-01-10 17:56:54 -0200

    Muda ordem de operações de acordo com a blocagem
    Se o cpareia for ler a blocagem do arquivo, nós precisamos esperar que a
    leitura acabe, já que ele pode tentar acessar um registro que ainda não
    foi lido. Além disso, o número de threads neste caso é apenas um na
    blocagem, então precisamos liberá-la diferente do caso tradicional, onde
    temos n - 1 threads blocando.

--- cpareia.c
+++ cpareia.c
@@ -12,38 +12,37 @@ main(int argc, char *argv[]) {
   pthread_t *read_thread;
   pthread_t **blocking_threads;
   pthread_t **comparator_threads;
-  args_t *args;
-
-  args = args_new();
-
-  args_parse(args, argc, argv);
 
   project = project_new();
 
-  project_parse(project, args->project_file);
+  args_parse(project->args, argc, argv);
+
+  project_parse(project, project->args->project_file);
 
   printf("Começando leitura e blocagem\n");
   read_thread = database_read_async(project->d0);
 
-  if(args->blocking_file) {
+  if(project->args->blocking_file) {
+    pthread_join(*read_thread, NULL);
     blocking_threads = blocking_read_file_async(project);
+    pthread_join(*blocking_threads[0], NULL);
+    free(blocking_threads[0]);
   } else {
-    blocking_threads = blocking_async(project, args->max_threads - 1);
-  }
+    blocking_threads = blocking_async(project);
+    pthread_join(*read_thread, NULL);
 
-  pthread_join(*read_thread, NULL);
-
-  for(i = 0; i < args->max_threads - 1; i++) {
-    pthread_join(*blocking_threads[i], NULL);
-    free(blocking_threads[i]);
+    for(i = 0; i < project->args->max_threads - 1; i++) {
+      pthread_join(*blocking_threads[i], NULL);
+      free(blocking_threads[i]);
+    }
   }
 
   free(blocking_threads);
   free(read_thread);
   printf("Blocagem pronta\n\nComeçando comparação e escrita\n");
-  comparator_threads = comparator_run_async(project, args->max_threads);
+  comparator_threads = comparator_run_async(project);
 
-  for(i = 0; i < args->max_threads - 1; i++) {
+  for(i = 0; i < project->args->max_threads - 1; i++) {
     pthread_join(*comparator_threads[i], NULL);
     free(comparator_threads[i]);
   }
