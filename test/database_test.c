b23bf57d71030c3ac048c523777507782e21a7d6
Michel Boaventura <michel.boaventura@gmail.com>
2016-07-25 10:07:24 -0300

    Teste de db

--- test/database_test.c
+++ test/database_test.c
@@ -4,12 +4,33 @@ void
 database_test() {
   database_t *database;
   char *filename;
+  record_t *record;
 
   filename = strdup("fixtures/database.csv");
 
   database = database_new(filename, 4, 3, ',');
 
   database_read(database);
+
+  assert_int_equal(array_size(database->records), 3);
+
+  record = array_get(database->records, 0);
+  assert_string_equal(record_get_field(record, 0), "field1");
+  assert_string_equal(record_get_field(record, 1), "field2");
+  assert_string_equal(record_get_field(record, 2), "field3");
+  assert_string_equal(record_get_field(record, 3), "field4");
+
+  record = array_get(database->records, 1);
+  assert_string_equal(record_get_field(record, 0), "foo1");
+  assert_string_equal(record_get_field(record, 1), "foo2");
+  assert_string_equal(record_get_field(record, 2), "foo3");
+  assert_string_equal(record_get_field(record, 3), "foo4");
+
+  record = array_get(database->records, 2);
+  assert_string_equal(record_get_field(record, 0), "bar1");
+  assert_string_equal(record_get_field(record, 1), "bar2");
+  assert_string_equal(record_get_field(record, 2), "bar3");
+  assert_string_equal(record_get_field(record, 3), "bar4");
 
   database_free(database);
 }
